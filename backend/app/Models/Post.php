1d25548d21db8f3cb470725be1572fd381f34945
David <david-gelmut@rambler.ru>
2024-10-22 21:38:23 +0500

    Создание обновление и правки проекта.

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable  = [
 
-
+    protected $fillable = [
+        'title',
+        'description',
+        'prev_image',
+        'preview',
     ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class)->orderBy('created_at');
+    }
 }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AdminUser;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+          Post::factory(10)->create();
+          AdminUser::factory(1)->create([
+              'name'=>'Admin',
+              'email'=>'admin@admin.ru',
+              'password'=>bcrypt('12345')
+          ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
